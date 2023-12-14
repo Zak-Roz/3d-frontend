92b2db12dbfe49a161d7a3914c1e423a72b75fd2
Dmytro <lapota2015@gmail.com>
2023-12-14 20:33:35 +0200

    UI improvments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,16 @@ import { useReactMediaRecorder } from 'react-media-recorder';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import logo from './../assets/logo.png';
 
 export default function Home() {
   const [type, setType] = useState({type: TYPE.Idle, typePrev: null});
   const [iss, setiss] = useState(false);
   const navigate = useNavigate();
 
-  const onClick = ({ type, data }) =>
-    navigate('/workout', { state: { type, data } });
+    const onClick = ({type, data}) =>
+        navigate('/workout', {state: {type, data}});
+    const [isActive, setIsActive] = useState(false);
 
   const { status, startRecording, stopRecording, mediaBlobUrl } =
     useReactMediaRecorder({
@@ -41,115 +43,127 @@ export default function Home() {
       },
     });
 
-  const handleStartRecording = useCallback(() => {
-    setType({type: TYPE.Listening, typePrev: TYPE.Idle});
-    setiss(true)
-    startRecording();
-    setTimeout(() => {
-      setType({type: TYPE.Idle, typePrev: TYPE.Listening});
-      setiss(false)
-      stopRecording();
-    }, 5000);
-  }, [])
+    const handleStartRecording = useCallback(() => {
+        setIsActive(!isActive);
+        setType({type: TYPE.Listening, typePrev: TYPE.Idle});
+        setiss(true)
+        startRecording();
+        setTimeout(() => {
+            setType({type: TYPE.Idle, typePrev: TYPE.Listening});
+            setiss(false)
+            stopRecording();
+        }, 5000);
+    }, [])
 
-  return (
-    <div
-      className="container"
-      style={{
-        height: '60vh',
-        margin: 0,
-        marginLeft: '14%',
-        position: 'absolute',
-        top: '50%',
-        msTransform: 'translateY(-50%)',
-        transform: 'translateY(-50%)',
-      }}
-    >
-      <div className="row h-100">
-        <div className="col">
-          <div
-            onClick={() => onClick({ type: 'legs' })}
-            className="h-100 border bg-light d-flex align-items-center justify-content-center"
-            style={{
-              color: 'white',
-              backgroundPosition: 'center',
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat',
-              backgroundImage:
-                'url("https://liftmanual.com/wp-content/uploads/2023/05/Best-Leg-Workouts-for-Speed.png")',
-            }}
-          >
-            <strong>LEGS</strong>
-          </div>
-        </div>
-        <div className="col">
-          <div
-            onClick={() => onClick({ type: 'back' })}
-            className="h-100 border bg-light d-flex align-items-center justify-content-center"
-            style={{
-              color: 'white',
-              backgroundPosition: 'center',
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat',
-              backgroundImage:
-                'url("https://i0.wp.com/www.strengthlog.com/wp-content/uploads/2023/06/Back-exercises.jpg?fit=2304%2C1536&ssl=1")',
-            }}
-          >
-            <strong>BACK</strong>
-          </div>
-        </div>
-        <div className="col">
-          <div
-            onClick={() => onClick({ type: 'body' })}
-            className="h-100 border bg-light d-flex align-items-center justify-content-center"
-            style={{
-              color: 'white',
-              backgroundPosition: 'center',
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat',
-              backgroundImage:
-                'url("https://www.transparentlabs.com/cdn/shop/articles/how_long_muscle_1200x1200.jpg?v=1602607728")',
-            }}
-          >
-            <strong>BODY</strong>
-          </div>
-        </div>
-        <div className="col">
-          <div className="row h-100">
-            <div className="col-12">
-              <div className="h-50 border bg-light d-flex align-items-center justify-content-center">
-                <Canvas
-                  camera={{ position: [3, 5, 24], fov: 5 }}
-                  style={{
-                    backgroundColor: '#111a21',
-                    width: '300px',
-                    height: '400px',
-                  }}
-                >
-                  <ambientLight intensity={1.25} />
-                  <ambientLight intensity={0.1} />
-                  <directionalLight intensity={0.4} />
-                  <Suspense fallback={null}>
-                    <Model position={[0, -1, 0]} type={type.type} typePrev={type.typePrev} />
-                  </Suspense>
-                  <OrbitControls autoRotate={false} />
-                </Canvas>
-              </div>
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <nav className="navbar navbar-light bg-secondary">
+                    <div className="container-fluid align-items-center justify-content-center"
+                         style={{
+                             userSelect: 'none',
+                         }}>
+                        <a className="navbar-brand" href="">
+                            <img src={logo} alt="" width="150" height="75" className="d-inline-block align-text-top"/>
+                        </a>
+                    </div>
+                </nav>
             </div>
-            <div className="col-12">
-              <div className="h-50 d-flex align-items-center justify-content-center">
-                <button
-                  onClick={handleStartRecording}
-                  className="btn btn-lg "
-                  style={{ backgroundColor: '#4caf50', color: 'white' }}
-                >
-                  Start Recording
-                </button>
-              </div>
+
+            <div className="row mt-5">
+                <div className="col ">
+                    <div className="container muscle-groups-container">
+                        <div className="row mt-1 muscle-group-label">
+                            <div className="col d-flex align-items-center justify-content-center ">
+                                <h5>MUSCLE GROUPS</h5>
+                            </div>
+                        </div>
+                        <div className="row h-100 mt-4">
+                            <div className="col card-column">
+                                <div
+                                    onClick={() => onClick({type: 'legs'})}
+                                    className="muscle-group-card h-100 border bg-light d-flex align-items-center justify-content-center rounded-5 hover-effect"
+                                    style={{
+                                        color: 'white',
+                                        backgroundPosition: 'center',
+                                        backgroundSize: 'cover',
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundImage:
+                                            'url("https://liftmanual.com/wp-content/uploads/2023/05/Best-Leg-Workouts-for-Speed.png")',
+                                        userSelect: 'none',
+                                    }}>
+                                    <strong>LEGS</strong>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div
+                                    onClick={() => onClick({type: 'back'})}
+                                    className="muscle-group-card h-75 border bg-light d-flex align-items-center justify-content-center rounded-5 hover-effect"
+                                    style={{
+                                        color: 'white',
+                                        backgroundPosition: 'center',
+                                        backgroundSize: 'cover',
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundImage:
+                                            'url("https://i0.wp.com/www.strengthlog.com/wp-content/uploads/2023/06/Back-exercises.jpg?fit=2304%2C1536&ssl=1")',
+                                        userSelect: 'none',
+                                    }}>
+                                    <strong>BACK</strong>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div
+                                    onClick={() => onClick({type: 'body'})}
+                                    className="muscle-group-card h-75 border bg-light d-flex align-items-center justify-content-center rounded-5 hover-effect"
+                                    style={{
+                                        color: 'white',
+                                        backgroundPosition: 'center',
+                                        backgroundSize: 'cover',
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundImage:
+                                            'url("https://www.transparentlabs.com/cdn/shop/articles/how_long_muscle_1200x1200.jpg?v=1602607728")',
+                                        userSelect: 'none',
+                                    }}>
+                                    <strong>BODY</strong>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="col-4 ">
+                    <div className="row">
+                        <div className="col  d-flex justify-content-center">
+                            <Canvas className="rounded-5 shadowed"
+                                    camera={{position: [3, 5, 24], fov: 5}}
+                                    style={{
+                                        backgroundColor: '#111a21',
+                                        width: '300px',
+                                        height: '400px',
+                                    }}
+                            >
+                                <ambientLight intensity={1.25}/>
+                                <ambientLight intensity={0.1}/>
+                                <directionalLight intensity={0.4}/>
+                                <Suspense fallback={null}>
+                                    <Model position={[0, -1, 0]} type={type.type} typePrev={type.typePrev}/>
+                                </Suspense>
+                                <OrbitControls autoRotate={false}/>
+                            </Canvas>
+                        </div>
+                    </div>
+                    <div className="row mt-4">
+                        <div className="col d-flex justify-content-center">
+                            <div
+                                className={`voice-button ${isActive ? 'active' : ''}`}
+                                onClick={handleStartRecording}
+                            >
+                                <FontAwesomeIcon icon={isActive ? faStop : faMicrophone}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState, Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import React, {useEffect, useState, Suspense} from 'react';
+import {Canvas} from '@react-three/fiber';
+import {OrbitControls} from '@react-three/drei';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import { useLocation } from 'react-router-dom';
-import { Model, TYPE } from '../Fit_guy';
+import {useLocation} from 'react-router-dom';
+import {Model, TYPE} from '../Fit_guy';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import logo from "../assets/logo.png";
+import { Dropdown } from 'react-bootstrap';
 
 export default function Workout() {
   const {
@@ -52,55 +54,103 @@ export default function Workout() {
     return TYPE.some;
   }
 
-  return [...data, ...items].map((item) => (
-    <div className="list-group align-items-center justify-content-center mt-2">
-          {show && item.id === id ?
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header>
-          <Canvas
-            camera={{ position: [3, 5, 24], fov: 7 }}
-            style={{
-              backgroundColor: '#111a21',
-              height: '500px',
-              width: '500px',
-            }}
-          >
-            <ambientLight intensity={1.25} />
-            <ambientLight intensity={0.1} />
-            <directionalLight intensity={0.4} />
-            <Suspense fallback={null}>
-              <Model position={[0, -0.8, 0]} type={getType(item.muscleGroup)} />
-            </Suspense>
-            <OrbitControls autoRotate />
-          </Canvas>
-        </Modal.Header>
-      </Modal>
-           : null}
-      <div className="card mb-3" style={{ maxWidth: '800px' }} onClick={() => handleShow(item.id)}>
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img
-              src={
-                item?.file?.link ||
-                'https://liftmanual.com/wp-content/uploads/2023/05/Best-Leg-Workouts-for-Speed.png'
-              }
-              className="img-fluid rounded-start"
-              alt={item?.file?.name || 'Some name'}
-            />
-            
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">{item.name || 'Some name'}</h5>
-              <p className="card-text">{item.descriptions || 'Some name'}</p>
-              <p className="card-text">Level: {item.level || 'Some name'}</p>
-              <p className="card-text">
-                Muscle Group: {item.muscleGroup || 'Some name'}
-              </p>
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <nav className="navbar navbar-light bg-secondary">
+                    <div className="container-fluid align-items-center justify-content-center"
+                         style={{
+                             userSelect: 'none',
+                         }}>
+                        <a className="navbar-brand" href="">
+                            <img src={logo} alt="" width="150" height="75" className="d-inline-block align-text-top"/>
+                        </a>
+                    </div>
+                </nav>
             </div>
-          </div>
+            <div className="container exercises-container">
+                <div className="row mt-1 muscle-group-label">
+                    <div className="col d-flex align-items-center justify-content-center">
+                        <h5>REQUESTED EXERCISES</h5>
+                    </div>
+                </div>
+                <div className="container exercise-card-container">
+                    {([...data, ...items]).reduce((rows, item, index) => {
+                        const rowIndex = Math.floor(index / 3);
+                        const colIndex = index % 3;
+
+                        if (!rows[rowIndex]) {
+                            rows[rowIndex] = [];
+                        }
+                        rows[rowIndex].push(
+                            <div className="col-4 mt-1 mb-1" key={index}>
+                                {show && item.id === id ?
+                                    <Modal show={show} onHide={handleClose}>
+                                        <Modal.Title className="modal-title">Perfomance example: {item.name || 'Default Title'}</Modal.Title>
+                                        <Modal.Header>
+                                            <Canvas className="modal-body"
+                                                camera={{ position: [3, 5, 24], fov: 7 }}
+                                                style={{
+                                                    backgroundColor: '#111a21',
+                                                    height: '500px',
+                                                    width: '500px',
+                                                }}>
+                                                <ambientLight intensity={1.25} />
+                                                <ambientLight intensity={0.1} />
+                                                <directionalLight intensity={0.4} />
+                                                <Suspense fallback={null}>
+                                                    <Model position={[0, -0.8, 0]} type={getType(item.muscleGroup)} />
+                                                </Suspense>
+                                                <OrbitControls autoRotate />
+                                            </Canvas>
+                                        </Modal.Header>
+                                    </Modal>
+                                    : null}
+                                <div className="card mb-3 exercise-card" style={{ maxWidth: '800px', height: '100%', width: '100%' }} >
+                                    <div className="row g-0">
+                                        <div className="col-md-4">
+                                            <img
+                                                src={
+                                                    item?.file?.link ||
+                                                    'https://liftmanual.com/wp-content/uploads/2023/05/Best-Leg-Workouts-for-Speed.png'
+                                                }
+                                                className="img-fluid rounded-start m-1"
+                                                alt={item?.file?.name || 'Some name'}
+                                                onClick={() => handleShow(item.id)}
+                                            />
+                                        </div>
+                                        <div className="col-md-8">
+                                            <div className="card-body">
+                                                <h5 className="card-title" onClick={() => handleShow(item.id)}>{item.name || 'Some name'}</h5>
+                                                <p className="card-text" onClick={() => handleShow(item.id)}>
+                                                    Muscle Group: {item.muscleGroup || 'Some name'}
+                                                </p>
+                                                <p className="card-text" onClick={() => handleShow(item.id)}>Level: {item.level || 'Some name'}</p>
+                                                <Dropdown>
+                                                    <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                                                        Show Description
+                                                    </Dropdown.Toggle>
+
+                                                    <Dropdown.Menu>
+                                                        <Dropdown.ItemText className="dropdown-item-text">
+                                                            {item.descriptions || 'Some name'}
+                                                        </Dropdown.ItemText>
+                                                    </Dropdown.Menu>
+                                                </Dropdown>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                        return rows;
+                    }, []).map((row, rowIndex) => (
+                        <div className="row" key={rowIndex}>
+                            {row}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  ));
+    );
 }
